b82d6b855a3e6b4c2cca5fe53ab2e30629c64075
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-04-30 12:38:43 +0530

    Delete sum of all values in specific field.sql

--- PLAYER/sum/sum of all values in specific field.sql
+++
@@ -1,6 +0,0 @@
-create database company;
-create table employee(id integer,cost varchar(25),experience varchar(30),sex char(1));
-insert into employee(id,cost,experience,sex) values(123,'@gmail.com','15bmc044','f');
-insert into employee(id,cost,experience,sex) values(123,'@gmail.com','15bmc044','m');
-insert into employee(id,cost,experience,sex) values(123,'@gmail.com','15bmc044','f');
-select sum(id) from employee ;
