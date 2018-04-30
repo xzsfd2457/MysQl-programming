c71c695bb6db353caeea5cda3011c8a40326e72d
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-04-30 11:20:09 +0530

    Create Delete one table from database.sql

---
+++ BEGINEER/Delete one table from database.sql
@@ -0,0 +1,5 @@
+create database student;
+create table one(email varchar(25),id varchar(10),roll varchar(30));
+create table two(email varchar(25),id varchar(10),roll varchar(30));
+drop table two;
+show databases;
