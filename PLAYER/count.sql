c5e09a313ac1dfb94b738bd32d4f24f1e7326189
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-04-30 12:10:34 +0530

    Create count.sql

---
+++ PLAYER/count.sql
@@ -0,0 +1,4 @@
+create database company;
+create table employee(id integer,cost varchar(25),experience varchar(30));
+insert into employee(id,cost,experience) values(123,'@gmail.com','15bmc044');
+select count(*) from employee;
