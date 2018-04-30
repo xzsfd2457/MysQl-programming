9499e73bad211cdcfc47da86ec3472bbcddad053
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-04-30 12:14:21 +0530

    Update count.sql

--- PLAYER/count.sql
+++ PLAYER/count.sql
@@ -2,3 +2,9 @@ create database company;
 create table employee(id integer,cost varchar(25),experience varchar(30));
 insert into employee(id,cost,experience) values(123,'@gmail.com','15bmc044');
 select count(*) from employee;
+
+for counting specific field:
+create database company;
+create table employee(id integer,cost varchar(25),experience varchar(30));
+insert into employee(id,cost,experience) values(123,'@gmail.com','15bmc044');
+select count(*),count(id) from employee;
