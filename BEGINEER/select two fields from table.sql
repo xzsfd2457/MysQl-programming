00e56f70747ae43fc9e16bc6ed2fd2f8098cb199
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-04-30 10:21:51 +0530

    Create select two fields from table.sql

---
+++ BEGINEER/select two fields from table.sql
@@ -0,0 +1,4 @@
+create database dk;
+create table dk(email varchar(25),id varchar(10),roll varchar(30));
+insert into dk(email,id) values('@gmail.com','15bmc044','dk@123');
+select id,email from dk;
