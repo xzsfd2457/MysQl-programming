29dbe6e0e18786296546df1de2c47093fa3a3424
xzsfd2457 <37336692+xzsfd2457@users.noreply.github.com>
2018-05-01 10:14:00 +0530

    Create delete.sql

---
+++ PLAYER/delete.sql
@@ -0,0 +1,9 @@
+create database company;
+create table employee(id integer,cost varchar(25),experience varchar(30),sex char(1));
+insert into employee(id,cost,experience,sex) values(123,'@gmail.com','15bmc044','f');
+insert into employee(id,cost,experience,sex) values(123,'@gmail.com','15bmc041','m');
+insert into employee(id,cost,experience,sex) values(12,'@gmail.com','15bmc042','f');
+DELETE from employee where id =123;
+select*from employee;
+
+
